6324b7e7bb08d6d0183727681e2c6096c79709f9
prachi-patil <40818070+prachi-patil@users.noreply.github.com>
2018-07-04 12:27:11 +0530

    Create CricketCoach.java

---
+++ src/com/springDemo/CricketCoach.java
@@ -0,0 +1,43 @@
+package com.springDemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
+	
+	public CricketCoach() {
+		System.out.println("Cricket constructor");
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Cricket setter method");
+		this.fortuneService = fortuneService;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 mins";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+	
+	
+	public void showDetails() {
+		System.out.println("My email is : "+emailAddress);
+		System.out.println("I represent "+team);
+	}
+
+}
